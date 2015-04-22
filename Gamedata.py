dbbb2637c89977184c0e3b4c06702eff198e32c1
Brendan O'Dowd <bodowd@worcester.edu>
2015-04-22 10:18:42 -0400

    Change output filename format

--- Gamedata.py
+++ Gamedata.py
@@ -2,6 +2,11 @@ from bs4 import BeautifulSoup
 
 class Gamedata(): 
 
-def __init__(self):
+	def __init__(self):
+		for arg in sys.argv[1:]
+			with open(arg,"r") as fo:
+				self.gamelist = fo.readlines[2:] 
 
-if __name__ == '__main__':
+	if __name__ == '__main__'
+		for i in readlines:
+		
--- logscrape.py
+++ logscrape.py
@@ -4,21 +4,21 @@ import sys
 from urllib2 import Request
 from bs4 import BeautifulSoup
 
-class Logscrape():
+class Gamelogs():
 
 	def __init__(self):
 
 		self.teamPageSoup = BeautifulSoup(urllib2.urlopen('http://www.sports-reference.com/cbb/schools/' + school + '/2015-gamelogs.html'))
 		self.statusPageLinks = self.teamPageSoup.findAll(href=re.compile("boxscores"));
 
-	def printLinks(school):
-		logs = Logscrape()
-		fo = open(school + 'links.txt',"w")
-		fo.write('\n'.join(str(l) for l in logs.statusPageLinks))
-		fo.close
+def printLinks(school):
+	gamelogs = Gamelogs()
+	fo = open(school + '.txt',"w")
+	fo.write('\n'.join(str(l) for l in gamelogs.statusPageLinks))
+	fo.close
 
-	if __name__ == '__main__':
-		for arg in sys.argv[1:]:
-			school = arg
-			printLinks(school)
+if __name__ == '__main__':
+	for arg in sys.argv[1:]:
+		school = arg	
+		printLinks(school)
 	
