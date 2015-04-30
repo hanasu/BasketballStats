23d9f7a9dac20f2b5c29500bcdf28cf3c8ddc5a3
Brendan O'Dowd <bodowd@worcester.edu>
2015-04-30 14:06:19 -0400

    Duplicates removed, filenames corrected, full season boxscore data scraped and saved locally

--- logscrape.py
+++ logscrape.py
@@ -13,24 +13,23 @@ class Gamelogs():
 		#use regex to only find links with score data		
 		self.statusPageLinks = self.teamPageSoup.findAll(href=re.compile("boxscores"));
 
-def scoredata(links, school):
+def scoredata(links, school):	
+	outputDirectory = '/home/hanasu/Desktop/NCAABBallData/' + school
+	#check if school directory exists, if not make it
+	if not (os.path.exists(outputDirectory)): 
+		os.makedirs(outputDirectory)
 	#for each link in the school's season	
 	for filename in links:
-		print filename
 		gameSoup = BeautifulSoup(urllib2.urlopen(filename))
 		#remove extra link formatting to get just filename alone
-		filename = filename[38+len(school):]
+		filename = filename[28+len(school):]
 		#create a list that will hold the box score data only	
 		output = gameSoup.findAll(class_="sortable")
-		outputDirectory = '/home/hanasu/Desktop/NCAABBallData/' + school
-		#check if school directory exists, if not make it
-		if not (os.path.exists(outputDirectory)): 
-			os.makedirs(outputDirectory)
 		#conjoin the path with the filename for all OS versions	
 		filename = os.path.join(outputDirectory, filename)
 		#open a local file with that filename to store the results
 		fo = open(filename,"w")
-		#write it line by line to the file that was just opened	
+		#write output line by line to the file that was just opened	
 		for o in output:
 			fo.write(str(o) + '\n')
 		fo.close
@@ -38,17 +37,19 @@ def scoredata(links, school):
 def getlinks(school):
 	gamelogs = Gamelogs()
 	#remove extraneous links
-	gamelogs.statusPageLinks = gamelogs.statusPageLinks[2:]
+	gamelogs.statusPageLinks = gamelogs.statusPageLinks[2:]	
+	#remove duplicate links	
+	gamelogs.statusPageLinks = set(gamelogs.statusPageLinks) 
 	#create the list that will hold each school's seasonlong boxscores
 	boxlinks = list()
-	for s in gamelogs.statusPageLinks:
+	for s in gamelogs.statusPageLinks:		
 		#make the list element a string so it can be sliced
 		string = str(s)
-		#remove extra link formatting
-		string = string[9:]
+		#remove extra HTML link formatting
+		string = string[24:]
 		string = string[:-16]
 		#create the full list of games per school
-		boxlinks.insert(0, 'http://www.sports-reference.com' + string)
+		boxlinks.insert(0, 'http://www.sports-reference.com/cbb/boxscores/' + string)
 	scoredata(boxlinks, school)		
 
 if __name__ == '__main__':
